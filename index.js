f7aa522f5a25b0662482cbd99fea58f67183fb31
maryamr25 <maryamrasheed0987@gmail.com>
2024-03-01 22:06:45 +0500

    task-1

---
+++ index.js
@@ -0,0 +1,32 @@
+//1.Welcome Message:
+// function displayWelcomeMessage() {
+//   let name = prompt("Enter your name:");
+//   alert("Welcome  " + name + "!");
+
+// }
+// displayWelcomeMessage();
+
+//2.Quote Generator:
+// let quotes = [
+    //  "The only way to do great work is to love what you do. - Steve Jobs",
+    //  "Don't be afraid to give up the good to go for the great. - John D. Rockefeller",
+    //  "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+    //  "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt"];
+    
+//     function generateQuote() {
+//      let   quotesIndexes = Math.floor(Math.random() * quotes.length);
+//         let randomQuote = quotes[quotesIndexes];
+//         alert(randomQuote);   
+//     }
+//     generateQuote();
+    
+
+   //3. Random Number Generator:
+                                        
+    
+ function generateRandomNumber() {
+     let randomNumber = Math.floor(Math.random() * 100) + 1;
+        alert(randomNumber);
+ }
+ generateRandomNumber();
+    
